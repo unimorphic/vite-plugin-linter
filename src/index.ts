116f9a725df3c36476aa6c3458a5ca317efce0ad
Unimorphic <thowlett@unimorphic.ca>
2021-05-24 20:34:05 -0600

    Fixing types & build

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 export type { FilterPattern } from "@rollup/pluginutils";
 export type { default as Linter, LinterResult } from "./Linter";
-export type {
-  default as linterPlugin,
-  LinterPluginOptions,
-  LinterResultData,
-} from "./linterPlugin";
-export type { default as EsLinter, EsLinterOptions } from "./linters/EsLinter";
+export { default as linterPlugin } from "./linterPlugin";
+export type { LinterPluginOptions, LinterResultData } from "./linterPlugin";
+export { default as EsLinter } from "./linters/EsLinter";
+export type { EsLinterOptions } from "./linters/EsLinter";
 export { default as TypeScriptLinter } from "./linters/TypeScriptLinter";
 export type { normalizePath, onlyUnique } from "./utils";
